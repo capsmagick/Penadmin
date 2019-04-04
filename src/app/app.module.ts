6412f37782f1990a83381928d908ca02961c4f48
renoycb <renoycb@gmail.com>
2019-04-04 22:01:34 +0530

    3 files modified, 5 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { AdminPanelComponent } from './admin-panel/admin-panel.component';
 import { NewProductComponent } from './admin-panel/new-product/new-product.component';
+import { SideNavComponent } from './admin-panel/side-nav/side-nav.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, AdminPanelComponent, NewProductComponent ],
+  declarations: [ AppComponent, HelloComponent, AdminPanelComponent, NewProductComponent, SideNavComponent, AdminLoginComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
