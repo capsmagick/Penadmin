2ae12ef493a7298596740aea343c69c797951d14
renoycb <renoycb@gmail.com>
2019-04-04 21:13:39 +0530

    1 file modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { AdminPanelComponent } from './admin-panel/admin-panel.component';
+import { NewProductComponent } from './admin-panel/new-product/new-product.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, AdminPanelComponent, NewProductComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
